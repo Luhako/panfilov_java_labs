9818be4c86482fad3abc27a5545a5a9a4a4471cf
Luhako <55746002+Luhako@users.noreply.github.com>
2026-02-13 05:56:44 +0700

    переобновил MyReader.java
    я там баги фиксил наверное что-то поменялось

--- lab0/MyReader.java
+++ lab0/MyReader.java
@@ -6,30 +6,31 @@ import java.lang.*; // импорт полезностей 2 (IsLetterOrDigit м
 // implements AutoCloseable наследование или вроде того интерфейса, от которого наследуются штуки с "автодеструктором"
 // теперь надо переопределить метод close и он вызовется когда ресурс не будет нужен...
 public class MyReader implements AutoCloseable{ // изначально Reader, но такой класс уже есть в java?
-    private Reader reader = null; // ну да вот он класс для ввода символов
+    private Reader reader = null;
+    // ну да вот он класс для ввода символов
     // я же верно понимаю тут перед всем надо ставить уровень приватности?
 
      public MyReader(String filename){ // конструктор
-        try (FileInputStream fis = new FileInputStream(filename);){ //FileInputStream - класс читатель байтов файла
-            this.reader = new InputStreamReader(fis); // InputStreamReader - класс переводчик байтов в символы
+        try { //FileInputStream - класс читатель байтов файла
+            this.reader = new InputStreamReader(new FileInputStream(filename));// InputStreamReader - класс переводчик байтов в символы
             // Везде ли нужен this?
-            /* Подобное было в задании... открытие файла может выдать исключение, и, похоже, его надо ловить,
+            /* Подобное было в задании... задание файла может выдать исключение, и, похоже, его надо ловить,
         а не просто потом проверить открылось оно или нет, а теперь оно само закроется всё */
             //А что надо делать ещё в конструкторе кроме создания?
         }
-        catch (IOException e){ 
-        /* IOException — базовый класс для исключений,
+        catch (IOException e){
+            /* IOException — базовый класс для исключений,
         которые возникают при работе с файлами, каталогами и потоками.
         Ловим исключение открытия файла*/
-            System.err.println("Error while reading file: " + e.getLocalizedMessage()); 
+            System.err.println("Error while reading file: " + e.getLocalizedMessage());
             // вывод ошибок в ошибочный поток?
         }
     }
 
     public String getWord(){ // метод отдающий слова
          try { // я так понимаю вокруг любой работы с файлами надо писать try catch
-             char[] symvol = new char[0];
-             int coutOfSymvols = this.reader.read(symvol); // если -1 выдаст значит конец потока
+             char[] symvol = new char[1];
+             int coutOfSymvols = this.reader.read(symvol); // если -1 выдаст значит конец потока, а иначе даёт количество символов считанных
              while (!Character.isLetterOrDigit(symvol[0]) && coutOfSymvols >-1) { // идём по массиву пока не найдём слово
                  coutOfSymvols = this.reader.read(symvol);
              }
