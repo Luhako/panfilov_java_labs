a1231c00809da459b3ff848af0669a593de19480
Luhako <55746002+Luhako@users.noreply.github.com>
2026-02-13 03:25:28 +0700

    Версия MyReader первая норм.java
    сделан класс читающий слова из файла
    и ветка... я забыл сделать ветку похоже... можно ли это как-то поправить?

--- MyReader.java
+++ MyReader.java
@@ -1,37 +1,54 @@
 import java.io.*; // импорт всяких полезностей
+import java.lang.*; // импорт полезностей 2 (IsLetterOrDigit может ещё чего-то)
 
-public class MyReader { // изначально Reader, но мне кажется, или такой класс уже есть в java?
+// класс, который открывает файл и может возвращать слова
+
+// implements AutoCloseable наследование или вроде того интерфейса, от которого наследуются штуки с "автодеструктором"
+// теперь надо переопределить метод close и он вызовется когда ресурс не будет нужен...
+public class MyReader implements AutoCloseable{ // изначально Reader, но такой класс уже есть в java?
     private Reader reader = null; // ну да вот он класс для ввода символов
+    // я же верно понимаю тут перед всем надо ставить уровень приватности?
 
      public MyReader(String filename){ // конструктор
-        try{ /* Это было в задании... задание файла может выдать исключение, и, похоже, его надо ловить,
-        а не просто потом проверить открылось оно или нет */
-            reader = new InputStreamReader(new FileInputStream(filename)); // попытка открыть файл?
-        } /* FileInputStream - класс читатель байтов файла
-        InputStreamReader - класс переводчик байтов в символы
-        Запутанно? Но вроде что-то понятно...*/
-
-        catch (IOException e){ /* IOException — базовый класс для исключений,
+        try (FileInputStream fis = new FileInputStream(filename);){ //FileInputStream - класс читатель байтов файла
+            this.reader = new InputStreamReader(fis); // InputStreamReader - класс переводчик байтов в символы
+            // Везде ли нужен this?
+            /* Подобное было в задании... открытие файла может выдать исключение, и, похоже, его надо ловить,
+        а не просто потом проверить открылось оно или нет, а теперь оно само закроется всё */
+            //А что надо делать ещё в конструкторе кроме создания?
+        }
+        catch (IOException e){ 
+        /* IOException — базовый класс для исключений,
         которые возникают при работе с файлами, каталогами и потоками.
         Ловим исключение открытия файла*/
-            System.err.println("Error while reading file: " + e.getLocalizedMessage());
+            System.err.println("Error while reading file: " + e.getLocalizedMessage()); 
             // вывод ошибок в ошибочный поток?
         }
+    }
 
-        /*finally { //Код, который выполняется в другом случае
-            if (null != reader) // говорят это уже не актуально тк те классы сами освобождают ресурсы,
-            // но их надо указывать в аргументе к try, а говорили, что само память очищает,
-            // а ещё больше без вложенности!
-            {
-                try
-                {
-                    reader.close(); //Попытка закрыть если открыто
-                }
-                catch (IOException e) // Но ещё какое-то исключение?
-                {
-                    e.printStackTrace(System.err);
-                }
-            }
-        } */
+    public String getWord(){ // метод отдающий слова
+         try { // я так понимаю вокруг любой работы с файлами надо писать try catch
+             char[] symvol = new char[0];
+             int coutOfSymvols = this.reader.read(symvol); // если -1 выдаст значит конец потока
+             while (!Character.isLetterOrDigit(symvol[0]) && coutOfSymvols >-1) { // идём по массиву пока не найдём слово
+                 coutOfSymvols = this.reader.read(symvol);
+             }
+             String slovo = "";
+             if (coutOfSymvols == -1) return "";
+             while (Character.isLetterOrDigit(symvol[0]) && coutOfSymvols >-1){ // записываем слово
+                 slovo += symvol[0];
+                 coutOfSymvols = this.reader.read(symvol);
+             }
+             return slovo;
+         }
+         catch (IOException e){
+             System.err.println("Error while reading file: " + e.getLocalizedMessage());
+         }
+         return ""; // даже в случае ошибки нужно что-то вренуть?
+    }
+
+    @Override
+    public void close() throws IOException {
+        if (reader != null) reader.close(); //похоже одну строчку после условия можно и так написать без {}
     }
 }
