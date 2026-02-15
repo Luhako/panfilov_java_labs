7f515916906d6295eb742b93b21a2d1a4d48667c
Luhako <55746002+Luhako@users.noreply.github.com>
2026-02-15 21:53:06 +0700

    В CSVWriter.java убрана часть комментариев
    убраны те, которые объясняли что делают, функции java

--- lab0/CSVWriter.java
+++ lab0/CSVWriter.java
@@ -5,8 +5,6 @@ import java.util.Map;
 
 // класс, который открывает файл и может возвращать слова
 
-// implements AutoCloseable наследование или вроде того интерфейса, от которого наследуются штуки с "автодеструктором"
-// теперь надо переопределить метод close и он вызовется когда ресурс не будет нужен...
 public class CSVWriter implements AutoCloseable{
     private Writer writer = null;
     double WordsAtAll;
@@ -17,31 +15,22 @@ public class CSVWriter implements AutoCloseable{
             writer = new FileWriter("output.txt", false);
         }
         catch (IOException e){
-            /* IOException — базовый класс для исключений,
-        которые возникают при работе с файлами, каталогами и потоками.
-        Ловим исключение открытия файла*/
             System.err.println("Error while writing file: " + e.getLocalizedMessage());
-            // вывод ошибок в ошибочный поток
         }
     }
 
-    public void write(Map.Entry<String, Integer> wordAndFrequency){ //frequency = Частота по-английски
-        try { // я так понимаю вокруг любой работы с файлами надо писать try catch
+    public void write(Map.Entry<String, Integer> wordAndFrequency){ 
+        try { 
             writer.write(wordAndFrequency.getKey());
             writer.write(" ");
             writer.write("" + wordAndFrequency.getValue());
             writer.write(" ");
             writer.write("" + wordAndFrequency.getValue()/ WordsAtAll);
             writer.write("\n");
-            writer.flush(); // нужен для записи данных в файл или в буфер иначе они остаются
-            // там, где могут пропасть при закрытии потока
+            writer.flush(); 
         }
         catch (IOException e){
-            /* IOException — базовый класс для исключений,
-        которые возникают при работе с файлами, каталогами и потоками.
-        Ловим исключение открытия файла*/
             System.err.println("Error while writing file: " + e.getLocalizedMessage());
-            // вывод ошибок в ошибочный поток?
         }
     }
 
